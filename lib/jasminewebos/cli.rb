984a3904f850bfde821e913117373ac5359a6978
mike.gaffney <mike.gaffney@asolutions.com>
2009-09-17 13:47:02 -0500

    updating help

--- lib/jasminewebos/cli.rb
+++ lib/jasminewebos/cli.rb
@@ -14,15 +14,14 @@ module Jasminewebos
 
           Usage: #{File.basename($0)} 
           
-          Options: -p the path to the repo
+          Options:
         BANNER
         opts.separator ""
         opts.on("-p", "--path=PATH", String,
-                "This is a sample message.",
-                "For multiple lines, add more strings.",
+                "The path to the folder to jasmineize",
                 "Default: ~") { |arg| options[:path] = arg }
-#        opts.on("-h", "--help",
-#                "Show this help message.") { stdout.puts opts; exit }
+        opts.on("-h", "--help",
+                "Show this help message.") { stdout.puts opts; exit }
         opts.parse!(arguments)
 
         if mandatory_options && mandatory_options.find { |option| options[option.to_sym].nil? }
