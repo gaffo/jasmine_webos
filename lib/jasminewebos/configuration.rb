035dd0d645bb558e5b6eaf5cb8d53f0856c78fb8
mike.gaffney <mike.gaffney@asolutions.com>
2009-09-18 20:54:19 -0500

    working on cofnig

--- lib/jasminewebos/configuration.rb
+++ lib/jasminewebos/configuration.rb
@@ -1,10 +1,37 @@
 module Jasminewebos
   class Configuration
-    attr_accessor :port, :jasmine_root
+    attr_accessor :port, :jasmine_root, :application_root
     
     def initialize()
       @port = 8888
       @jasmine_root = File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "vendor", "jasmine"))
+    end
+    
+    def specs_dir
+      rase_error_if_no_application_root
+    end
+    
+    def matchers_dir
+      rase_error_if_no_application_root
+      
+    end
+    
+    def sources_json
+      rase_error_if_no_application_root
+      
+    end
+    
+    private
+    def rase_error_if_no_application_root
+      msg = <<EOF
+Application Root Configuration Not Set, 
+Please put the following in spec/jasminewebos.rb:
+-------------------------------------------------
+Jasminewebos.configure do |config|
+  config.application_root = File.join(File.dirname(__FILE__), "..")
+end
+EOF
+      raise Exception.new(msg)
     end
   end
 end
--- test/jasminewebos/test_configuration.rb
+++ test/jasminewebos/test_configuration.rb
@@ -13,6 +13,18 @@ class Jasminewebos::TestConfiguration < Test::Unit::TestCase
       assert_equal(File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "vendor", "jasmine")),
                    @config.jasmine_root)
     end
+    should "not have app root set" do
+      assert_nil(@config.application_root)
+    end
+    should "throw for sources.json with no app set" do
+      assert_raise(Exception){@config.sources_json}
+    end
+    should "throw for matchers with no app set" do
+      assert_raise(Exception){@config.matchers_dir}
+    end
+    should "throw for specs with no app set" do
+      assert_raise(Exception){@config.specs_dir}
+    end
   end
 
 end
